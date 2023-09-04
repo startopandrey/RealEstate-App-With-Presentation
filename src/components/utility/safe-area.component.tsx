c8c84ce7a09b97f63256a18560fc7a6f5ee8b6e2
Andrey Ilyukhin <startopandrey@gmail.com>
2023-09-04 15:20:14 +0300

    safe area isBottomHidden property added

--- src/components/utility/safe-area.component.tsx
+++ src/components/utility/safe-area.component.tsx
@@ -1,10 +1,13 @@
 import { StatusBar, SafeAreaView } from "react-native";
 import styled from "styled-components/native";
 
-export const SafeArea = styled(SafeAreaView)<{ transparent?: boolean }>`
+export const SafeArea = styled(SafeAreaView)<{
+  transparent?: boolean;
+  isBottomHidden?: boolean;
+}>`
   flex: 1;
   ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
   background-color: ${(props) =>
     props.transparent ? "transparent" : props.theme.colors.bg.primary};
-  margin-bottom: 80px;
+  margin-bottom: ${(props) => (props.isBottomHidden ? "0px" : "80px")};
 `;
