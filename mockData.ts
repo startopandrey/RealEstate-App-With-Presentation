f70b81048696318aaab4123bb9308222bafc314b
Andrey Ilyukhin <startopandrey@gmail.com>
2023-09-04 15:16:12 +0300

    modified mock for apartment

--- mockData.ts
+++ mockData.ts
@@ -1,4 +1,7 @@
-export const featuresListMock = [
+import { initialRegion } from "./src/utils/constants";
+import { ApartmentFeature, NewApartment } from "./src/types/apartments/apartment";
+
+export const featuresListMock: ApartmentFeature[] = [
   { type: "bedroom", quantity: 1 },
   { type: "bathroom", quantity: 1 },
   { type: "balcony", quantity: 1 },
@@ -20,3 +23,17 @@ export const facilitiesList = [
   { id: 7, name: "Kid’s Friendly" },
   { id: 8, name: "Pool" },
 ];
+export const initialNewApartment: NewApartment = {
+  title: "",
+  description: "",
+  category: null,
+  squareMeter: "",
+  location: initialRegion,
+  features: featuresListMock,
+  price: "",
+  address: "Property Address",
+  totalRooms: 1,
+  facilities: [],
+  photos: [],
+  authorId: "authorID",
+};
