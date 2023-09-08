6ffe6067d282ef0c7b0e726582dc7fe1965ec281
Andrey Ilyukhin <startopandrey@gmail.com>
2023-09-08 11:38:49 +0300

    register screen added

--- src/features/account/screens/register.screen.tsx
+++ src/features/account/screens/register.screen.tsx
@@ -1,36 +1,79 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 
 import { AccountStackNavigatorParamList } from "src/types/accout";
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { TouchableOpacity, View, Text as TextNative } from "react-native";
+import { TouchableOpacity } from "react-native";
 import {
   AuthSwitch,
   AuthContainer,
   AuthForm,
   FormRow,
-  AuthBannerWrapper,
-  AuthBanner,
   AuthSwitchText,
   AuthButton,
+  AuthBannerWrapper,
+  AuthBanner,
 } from "../components/account.styles";
+import { LATITUDE_DELTA, LONGITUDE_DELTA } from "../../../utils/constants";
 import { SafeArea } from "../../../components/utility/safe-area.component";
-import styled from "styled-components/native";
 import { Title } from "../../../components/title/title.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
 import { Text } from "../../../components/typography/text.component";
 import { Input } from "../../../components/input/input.component";
-import { Button } from "../../../components/button/button.component";
+import { isValidRegister } from "../../../utils/tests.test";
+import { IconButton } from "../../../components/icon-button/icon-button.component";
+import { initialRegion } from "../../../utils/constants";
+import {
+  getCurrentUserLoction,
+  isLocationPermission,
+} from "../../../services/helpers/location.helper";
+import { registerRequest } from "../../../services/authentication/authentication.service";
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
+import { Loading } from "../../../components/loading/loading.component";
 type Props = NativeStackScreenProps<AccountStackNavigatorParamList, "Login">;
 
 export const RegisterScreen = ({ navigation }: Props) => {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [userLocation, setUserLocation] = useState(initialRegion);
+  const [isPermissionsAccepted, setIsPermissionsAccepted] = useState(false);
   const [isPasswordShowed, setIsPasswordShowed] = useState(false);
-  // const { onLogin, error, isLoading } = useContext(AuthenticationContext);
+  const { onRegister, error, isLoading } = useContext(AuthenticationContext);
   const showPassword = () => {
     setIsPasswordShowed((pwd) => !pwd);
   };
+  const getCurrentLocation = async () => {
+    if (userLocation.latitude !== 0) {
+      return;
+    }
+    const currentLocation = await getCurrentUserLoction();
+
+    if (currentLocation) {
+      setUserLocation({
+        longitude: currentLocation?.coords?.latitude,
+        latitude: currentLocation?.coords?.longitude,
+        longitudeDelta: LONGITUDE_DELTA,
+        latitudeDelta: LATITUDE_DELTA,
+      });
+    }
+  };
+  const getPermission = async () => { 
+    const isLocationPermissionAccepted = await isLocationPermission();
+
+    if (isLocationPermissionAccepted) {
+      setIsPermissionsAccepted(true);
+      return;
+    }
+    setIsPermissionsAccepted(false);
+    return;
+  };
+  useEffect(() => {
+    if (!isPermissionsAccepted) {
+      getPermission();
+    }
+    getCurrentLocation();
+  }, []);
+
   return (
     <SafeArea isBottomHidden={true}>
       <AuthContainer>
@@ -64,6 +107,7 @@ export const RegisterScreen = ({ navigation }: Props) => {
           <Input
             setValue={setEmail}
             value={email}
+            textTransform={"lowercase"}
             iconName="mail-outline"
             keyboardType="email-address"
             placeholder={"Email"}
@@ -77,6 +121,7 @@ export const RegisterScreen = ({ navigation }: Props) => {
             keyboardType="default"
             placeholder={"Password"}
           />
+
           <Spacer position="top" size="large" />
           <FormRow>
             <TouchableOpacity>
@@ -88,8 +133,12 @@ export const RegisterScreen = ({ navigation }: Props) => {
               } password`}</Text>
             </TouchableOpacity>
           </FormRow>
-          <Spacer position="top" size="large" />
-          <AuthButton title="Register" onPress={() => null}></AuthButton>
+          <Spacer position="top" size="xl" />
+          <AuthButton
+            title="Register"
+            disabled={!isValidRegister(email, password, username)}
+            onPress={() => onRegister(email, password, userLocation, username)}
+          />
         </AuthForm>
         <AuthSwitch onPress={() => navigation.navigate("Login")}>
           <AuthSwitchText>
@@ -98,7 +147,7 @@ export const RegisterScreen = ({ navigation }: Props) => {
           </AuthSwitchText>
         </AuthSwitch>
       </AuthContainer>
+      <Loading isOpen={isLoading}></Loading>
     </SafeArea>
   );
 };
-
