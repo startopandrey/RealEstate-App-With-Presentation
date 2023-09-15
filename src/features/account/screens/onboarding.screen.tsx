723ce580343117ab74b69a63c26ade04e9c7d2f6
Andrey Ilyukhin <startopandrey@gmail.com>
2023-09-15 16:11:39 +0300

    wait untill photo arrive animation

--- src/features/account/screens/onboarding.screen.tsx
+++ src/features/account/screens/onboarding.screen.tsx
@@ -1,18 +1,19 @@
 import React, { useEffect, useRef, useState } from "react";
-import LottieView from "lottie-react-native";
 
 import { Spacer } from "../../../components/spacer/spacer.component";
 
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { AccountStackNavigatorParamList } from "src/types/accout";
+import {
+  AccountStackNavigatorParamList,
+  OnboardingBlockType,
+} from "../../../types/accout";
 import styled from "styled-components/native";
+
 import { SafeArea } from "../../../components/utility/safe-area.component";
 import { Button } from "../../../components/button/button.component";
 import { Title } from "../../../components/title/title.component";
-import { Text } from "../../../components/typography/text.component";
 import { IconButton } from "../../../components/icon-button/icon-button.component";
 import { Animated } from "react-native";
-import { TitleType } from "../../../components/title/title.d";
 import { initialOnboardingBlocks } from "../../../../mockData";
 import {
   SmallButton,
@@ -29,39 +30,17 @@ import {
   ProgressBarWrapper,
 } from "../components/account.styles";
 import { theme } from "../../../infrastructure/theme";
-type Props = NativeStackScreenProps<AccountStackNavigatorParamList, "Main">;
+type Props = NativeStackScreenProps<
+  AccountStackNavigatorParamList,
+  "Onboarding"
+>;
 
-interface OnboardingBlockType {
-  titleArray: TitleType[];
-  description: string;
-  photoUrl: string;
-  isSelected: boolean;
-}
-const SkipButton = () => {
-  return (
-    <SmallButton>
-      <SmallButtonText variant="body">Skip</SmallButtonText>
-    </SmallButton>
-  );
-};
 const OnboardingBody = styled.View`
   padding: ${(props) => props.theme.space[2]};
 `;
-const Header = () => {
-  return (
-    <HeaderWrapper>
-      <LogoWrapper>
-        <Logo source={require("../../../../assets/app_logo.png")} />
-      </LogoWrapper>
-      <Spacer position="right" size="large">
-        <SkipButton />
-      </Spacer>
-    </HeaderWrapper>
-  );
-};
-export const  OnboardingScreen = ({ navigation }: Props) => {
-  const progressBarWidth = 150;
 
+export const OnboardingScreen = ({ navigation }: Props) => {
+  const progressBarWidth = 150;
   const [onboardingBlocks, setOnboardingBlocks] = useState<
     OnboardingBlockType[]
   >(initialOnboardingBlocks);
@@ -75,18 +54,23 @@ export const  OnboardingScreen = ({ navigation }: Props) => {
 
   const mainRef = useRef(new Animated.Value(1))?.current;
   useEffect(() => {
-    if (onboardingBlocks) {
-      setSelectedBlock(onboardingBlocks.find((el) => el.isSelected));
+    if (onboardingBlocks.length) {
+      setSelectedBlock(
+        onboardingBlocks.find((el: OnboardingBlockType) => el?.isSelected) ??
+          initialOnboardingBlocks[0]
+      );
       setSelectedBlockIndex(onboardingBlocks.findIndex((el) => el.isSelected));
     }
   }, [onboardingBlocks]);
 
-  const fadeInOutAnim = () => {
+  const fadeOut = () => {
     Animated.timing(mainRef, {
       toValue: 0.4,
       duration: 400,
       useNativeDriver: false,
     }).start();
+  };
+  const fadeIn = () => {
     setTimeout(() => {
       Animated.timing(mainRef, {
         toValue: 1,
@@ -95,72 +79,82 @@ export const  OnboardingScreen = ({ navigation }: Props) => {
       }).start();
     }, 400);
   };
-  const changeBlock = (type: "increase" | "decrease") => {
-    fadeInOutAnim();
-    setTimeout(() => {
-      if (type == "increase") {
-        const newOnboardingBlocks = onboardingBlocks.map((el, i) =>
-          i !== selectedBlockIndex + 1
-            ? { ...el, isSelected: false }
-            : { ...el, isSelected: true }
-        );
 
-        setOnboardingBlocks(newOnboardingBlocks);
-        const newProgressWidth =
-          progressBarWidth *
-          ((selectedBlockIndex + 2) / onboardingBlocks.length);
-        Animated.timing(progress, {
-          toValue: newProgressWidth,
-          duration: 400,
-          useNativeDriver: false,
-        }).start();
-      } else {
-        const newProgressWidth =
-          progressBarWidth * (selectedBlockIndex / onboardingBlocks.length);
-        const newOnboardingBlocks = onboardingBlocks.map((el, i) =>
-          i !== selectedBlockIndex - 1
-            ? { ...el, isSelected: false }
-            : { ...el, isSelected: true }
-        );
-        setOnboardingBlocks(newOnboardingBlocks);
-        Animated.timing(progress, {
-          toValue: newProgressWidth,
-          duration: 400,
-          useNativeDriver: false,
-        }).start();
-      }
+  const increaseBlock = () => {
+    setTimeout(() => {
+      const newOnboardingBlocks = onboardingBlocks.map((el, i) =>
+        i !== selectedBlockIndex + 1
+          ? { ...el, isSelected: false }
+          : { ...el, isSelected: true }
+      );
+
+      setOnboardingBlocks(newOnboardingBlocks);
+      const newProgressWidth =
+        progressBarWidth * ((selectedBlockIndex + 2) / onboardingBlocks.length);
+      Animated.timing(progress, {
+        toValue: newProgressWidth,
+        duration: 400,
+        useNativeDriver: false,
+      }).start();
     }, 400);
   };
-
+  const decreaseBlock = () => {
+    setTimeout(() => {
+      const newProgressWidth =
+        progressBarWidth * (selectedBlockIndex / onboardingBlocks.length);
+      const newOnboardingBlocks = onboardingBlocks.map((el, i) =>
+        i !== selectedBlockIndex - 1
+          ? { ...el, isSelected: false }
+          : { ...el, isSelected: true }
+      );
+      setOnboardingBlocks(newOnboardingBlocks);
+      Animated.timing(progress, {
+        toValue: newProgressWidth,
+        duration: 400,
+        useNativeDriver: false,
+      }).start();
+    }, 400);
+  };
   const onPressNext = () => {
     if (selectedBlockIndex + 1 == onboardingBlocks.length) {
       navigation.navigate("Login");
       return;
     }
-    changeBlock("increase");
+    increaseBlock();
   };
   const onPressBack = () => {
-    changeBlock("decrease");
+    decreaseBlock();
   };
   return (
     <SafeArea isBottomHidden={true}>
       <Onboarding>
-        <Header />
+        <HeaderWrapper>
+          <LogoWrapper>
+            <Logo source={require("../../../../assets/app_logo.png")} />
+          </LogoWrapper>
+          <Spacer position="right" size="large">
+            <SmallButton onPress={() => navigation.navigate("Login")}>
+              <SmallButtonText variant="body">Skip</SmallButtonText>
+            </SmallButton>
+          </Spacer>
+        </HeaderWrapper>
         <Animated.View style={{ opacity: mainRef }}>
           <OnboardingBody>
             <Title titleArray={selectedBlock?.titleArray} />
-            <Spacer position="top" size="large"></Spacer>
+            <Spacer position="top" size="large" />
             <Description variant="body">
               {selectedBlock.description}
             </Description>
-            <Spacer position="top" size="large"></Spacer>
+            <Spacer position="top" size="large" />
           </OnboardingBody>
           <ApartmentPhotoWrapper>
             <ApartmentPhoto
               source={{
                 uri: selectedBlock.photoUrl,
               }}
-            ></ApartmentPhoto>
+              onLoadStart={() => fadeOut()}
+              onLoadEnd={() => fadeIn()}
+            />
           </ApartmentPhotoWrapper>
         </Animated.View>
         <NavigationOverlay>
@@ -172,20 +166,20 @@ export const  OnboardingScreen = ({ navigation }: Props) => {
                 height: 5,
                 borderRadius: theme.borderRadius.large,
               }}
-            ></Animated.View>
+            />
           </ProgressBarWrapper>
-          <Spacer position="top" size="large"></Spacer>
+          <Spacer position="top" size="large" />
           <OverlayButtons>
             {selectedBlockIndex !== 0 && (
               <Spacer position="right" size="large">
                 <IconButton
                   onPress={onPressBack}
                   iconName="arrow-back-outline"
-                ></IconButton>
+                />
               </Spacer>
             )}
 
-            <Button onPress={onPressNext} title="Next"></Button>
+            <Button onPress={onPressNext} title="Next" />
           </OverlayButtons>
         </NavigationOverlay>
       </Onboarding>
