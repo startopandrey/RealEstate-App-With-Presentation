8b69ab30f5a6e7229f8328e21a25fa103b41079e
Andrey Ilyukhin <startopandrey@gmail.com>
2023-09-15 16:19:17 +0300

    network log removed

--- src/utils/network.ts
+++ src/utils/network.ts
@@ -1,6 +1,6 @@
 import { useNetInfo } from "@react-native-community/netinfo";
 export const useCheckNetworkConnection = (): boolean => {
   const netInfo = useNetInfo();
-  console.log(netInfo);
+
   return netInfo.isConnected ?? false;
 };
