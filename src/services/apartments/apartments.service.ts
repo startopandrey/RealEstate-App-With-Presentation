35ae2aa008d402287a418f8573dd85a1b86d8ae9
Andrey Ilyukhin <startopandrey@gmail.com>
2023-09-22 00:06:21 +0300

    dynamic server url

--- src/services/apartments/apartments.service.ts
+++ src/services/apartments/apartments.service.ts
@@ -1,31 +1,32 @@
 import { serverUrl } from "../../utils/env";
 import { Location, NewApartment } from "../../types/apartments/apartment";
-import { mockApartments } from "../../../mockData";
 
-export const apartmentsRequest = async (location, filterOptions) => {
+import { mockApartments } from "../../../mockData";
+import axios from "axios";
+
+export const apartmentsRequest = async ( filterOptions) => {
   const mockApartment = new Promise((resolve, reject) => {
-    resolve(mockApartments);
+    resolve({ data: mockApartments });
   });
   const transformFilterOptions = (filter) => {
     if (filter.categoryId == 0) {
       delete filter.categoryId;
-      console.log(filter, "dfdf");
+     
       return filter ?? {};
     }
     return filter;
   };
-  console.log(filterOptions.categoryId, "filter");
   const queryString = new URLSearchParams(
     transformFilterOptions(filterOptions)
   ).toString();
-  console.log(queryString);
-  return false
-    ? fetch(
-        `https://realestate-mobileapp-ca10e0a2d5fa.herokuapp.com/api/apartment/filter?price=20,90`
-      ).then((res: Response) => {
-        return res.json();
-      })
-    : mockApartment;
+
+  try {
+    return await axios
+      .get(`${serverUrl}/api/apartment/filter?${queryString}`)
+      .then((res) => res);
+  } catch (error) {
+    return null;
+  }
 };
 export const isApartmentInRadius = (
   marker: Location,
@@ -36,6 +37,7 @@ export const isApartmentInRadius = (
   const kx = Math.cos((Math.PI * circle.latitude) / 180) * 111;
   const dx = Math.abs(circle.longitude - marker.longitude) * kx;
   const dy = Math.abs(circle.latitude - marker.latitude) * 111;
+
   return Math.sqrt(dx * dx + dy * dy) <= km;
 };
 export const apartmentsTransform = ({
@@ -45,15 +47,14 @@ export const apartmentsTransform = ({
   data: NewApartment[];
   location: Location;
 }): NewApartment[] => {
-  console.log(location, "tr");
+
   const camelizedApartments = data;
   const mappedResults = camelizedApartments.map((apartment: NewApartment) => {
     return apartment;
   });
-
+  
   const apartmentsResults = mappedResults.filter(
     (ap) => isApartmentInRadius(ap.location, location, 50) && ap
   );
-
   return apartmentsResults;
 };
